fe88f72718a83e7d34075bfcaae686864df284b5
Ervin, Jung <jung.ervin@gmail.com>
2026-05-23 08:39:50 +0200

    Use shared WebSocket request helper for locos API

--- client/src/api/locosWsApi.ts
+++ client/src/api/locosWsApi.ts
@@ -9,39 +9,24 @@ import type {
 } from "../../../common/src/types";
 
 import {
-  generateId,
-} from "../helpers";
-
-import {
-  wsApi,
-} from "../services/wsApi";
+  requestWsCommand,
+} from "./wsRequest";
 
 async function sendLocosCommand(
   action: LocosCommandAction,
   locos?: Loco[]
 ) {
-  const requestId = generateId();
-
-  const response = await wsApi.request(
+  return requestWsCommand(
     "locosCommand",
     {
-      requestId,
       action,
       ...(locos !== undefined
         ? { locos }
         : {}),
     },
     "locosResponse",
-    data => data.requestId === requestId
+    "Locos WebSocket command failed."
   );
-
-  if (!response.ok) {
-    throw new Error(
-      response.message ?? "Locos WebSocket command failed."
-    );
-  }
-
-  return response;
 }
 
 export async function getLocosWs(): Promise<Loco[]> {
