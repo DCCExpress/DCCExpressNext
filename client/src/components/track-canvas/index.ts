1e16875c309b0353bb52bc2cf35e48fe10b2b100
Ervin, Jung <jung.ervin@gmail.com>
2026-05-23 13:19:50 +0200

    Export TrackCanvas block loco picker

--- client/src/components/track-canvas/index.ts
+++ client/src/components/track-canvas/index.ts
@@ -1,6 +1,7 @@
 // client/src/components/track-canvas/index.ts
 
 export * from "./TrackCanvas.types";
+export * from "./TrackCanvasBlockLocoPicker";
 export * from "./TrackCanvasSignalAspectPopover";
 export * from "./trackCanvasDraw";
 export * from "./trackCanvasGeometry";
