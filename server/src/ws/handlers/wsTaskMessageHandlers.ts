54e954a4e2025d778ef4cb71134273c7c0d27b4c
Ervin, Jung <jung.ervin@gmail.com>
2026-05-23 15:36:14 +0200

    Handle direct startAllTasks websocket command

--- server/src/ws/handlers/wsTaskMessageHandlers.ts
+++ server/src/ws/handlers/wsTaskMessageHandlers.ts
@@ -180,6 +180,10 @@ export const handleTaskMessage: WsMessageHandler = async ({
       return true;
     }
 
+    case "startAllTasks":
+      await taskRuntimeStore.startAllTasks();
+      return true;
+
     case "finishAllTasks":
       await taskRuntimeStore.finishAllTasks();
       return true;
