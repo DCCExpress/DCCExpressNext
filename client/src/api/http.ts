e31c519168977c428b7332fb742bb033deb1cbdc
Ervin, Jung <jung.ervin@gmail.com>
2026-05-23 09:02:10 +0200

    Restore compatibility API facade

---
+++ client/src/api/http.ts
@@ -0,0 +1,20 @@
+export { getRouteGraph } from "./domainApi";
+export { getLocos, saveLocos } from "./domainApi";
+export { getLayout, saveLayout } from "./domainApi";
+export { getScript, saveScript } from "./domainApi";
+export {
+  addTrainTask,
+  deleteTrainTask,
+  getTaskManagerSnapshot,
+  pauseTrainTask,
+  reloadTrainTasks,
+  resumeTrainTask,
+  saveTrainTasks,
+  startAllTrainTasks,
+  startTrainTask,
+  finishAllTrainTasks,
+  finishTrainTask,
+  abortAllTrainTasks,
+  abortTrainTask,
+  updateTrainTask,
+} from "./domainApi";
