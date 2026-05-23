85f87d657a0bff3320d20b3b7327bd4c32903557
Ervin, Jung <jung.ervin@gmail.com>
2026-05-23 14:01:31 +0200

    Group TrackCanvas barrel exports

--- client/src/components/track-canvas/index.ts
+++ client/src/components/track-canvas/index.ts
@@ -1,22 +1,31 @@
 // client/src/components/track-canvas/index.ts
 
+// Core
 export * from "./TrackCanvas.types";
-export * from "./TrackCanvasBlockLocoPicker";
-export * from "./TrackCanvasSignalAspectPopover";
+export * from "./createCursorElement";
+
+// Rendering and geometry
+export * from "./trackCanvasDraw";
+export * from "./trackCanvasGeometry";
+export * from "./trackCanvasLayoutBounds";
+export * from "./trackCanvasSelection";
+export * from "./trackCanvasViewStorage";
+
+// Interaction
 export * from "./trackCanvasClickableActions";
 export * from "./trackCanvasCursor";
-export * from "./trackCanvasDraw";
 export * from "./trackCanvasEventListeners";
-export * from "./trackCanvasGeometry";
 export * from "./trackCanvasInteractionStop";
 export * from "./trackCanvasKeyboard";
-export * from "./trackCanvasLayoutBounds";
 export * from "./trackCanvasMouseDown";
 export * from "./trackCanvasMouseMove";
+export * from "./trackCanvasWheel";
+
+// Routes and signals
 export * from "./trackCanvasRouteActions";
-export * from "./trackCanvasSelection";
 export * from "./trackCanvasSignalAspect";
 export * from "./trackCanvasSignalPopoverState";
-export * from "./trackCanvasViewStorage";
-export * from "./trackCanvasWheel";
-export * from "./createCursorElement";
+
+// UI
+export * from "./TrackCanvasBlockLocoPicker";
+export * from "./TrackCanvasSignalAspectPopover";
