a602e016fe768d59a973326a68aeace0bc4b4334
Ervin, Jung <jung.ervin@gmail.com>
2026-05-24 09:42:37 +0200

    Add server runtime stats types

---
+++ common/src/serverRuntimeStats.ts
@@ -0,0 +1,15 @@
+// common/src/serverRuntimeStats.ts
+
+export type ServerRuntimeStatsSnapshot = {
+  timestamp: number;
+  memoryRssMb: number;
+  memoryHeapUsedMb: number;
+  memoryHeapTotalMb: number;
+  systemMemoryUsedMb: number;
+  systemMemoryTotalMb: number;
+  systemMemoryUsedPercent: number;
+  processCpuPercent: number | null;
+  systemLoadPercent: number | null;
+  cpuCount: number;
+  uptimeSec: number;
+};
