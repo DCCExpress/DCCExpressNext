b84b555ca1c2af7fdc4388a0ccdbaab470e00d6f
Ervin, Jung <jung.ervin@gmail.com>
2026-05-24 09:47:24 +0200

    Add server runtime stats hook

---
+++ client/src/hooks/useServerRuntimeStats.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+import type {
+  ServerRuntimeStatsSnapshot,
+} from "../../../common/src/types";
+
+import {
+  wsClient,
+} from "../services/wsClient";
+
+export function useServerRuntimeStats(): ServerRuntimeStatsSnapshot | null {
+  const [stats, setStats] =
+    useState<ServerRuntimeStatsSnapshot | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = wsClient.on<ServerRuntimeStatsSnapshot>(
+      "serverRuntimeStatsChanged",
+      data => {
+        setStats(data);
+      }
+    );
+
+    return unsubscribe;
+  }, []);
+
+  return stats;
+}
