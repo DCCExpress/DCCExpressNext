e628c5400f3a064af98d5010ce860c782ebc51e8
Ervin, Jung <jung.ervin@gmail.com>
2026-05-25 14:11:10 +0200

    Register German i18n resources

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -3,8 +3,10 @@ import { initReactI18next } from "react-i18next";
 
 import hu from "./i18n/hu.json";
 import en from "./i18n/en.json";
+import de from "./i18n/de.json";
 import signalLogicHu from "./i18n/signalLogic.hu.json";
 import signalLogicEn from "./i18n/signalLogic.en.json";
+import signalLogicDe from "./i18n/signalLogic.de.json";
 
 const savedLang = localStorage.getItem("lang") || "en";
 
@@ -18,10 +20,16 @@ const huTranslation = {
   ...signalLogicHu,
 };
 
+const deTranslation = {
+  ...de,
+  ...signalLogicDe,
+};
+
 i18n.use(initReactI18next).init({
   resources: {
     hu: { translation: huTranslation },
     en: { translation: enTranslation },
+    de: { translation: deTranslation },
   },
   lng: savedLang,
   fallbackLng: "en",
