14e23128b52fead1129505d826226f7660044b7b
Ervin, Jung <jung.ervin@gmail.com>
2026-05-25 10:59:42 +0200

    Load signal logic translation modules

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -3,13 +3,25 @@ import { initReactI18next } from "react-i18next";
 
 import hu from "./i18n/hu.json";
 import en from "./i18n/en.json";
+import signalLogicHu from "./i18n/signalLogic.hu.json";
+import signalLogicEn from "./i18n/signalLogic.en.json";
 
 const savedLang = localStorage.getItem("lang") || "en";
 
+const enTranslation = {
+  ...en,
+  ...signalLogicEn,
+};
+
+const huTranslation = {
+  ...hu,
+  ...signalLogicHu,
+};
+
 i18n.use(initReactI18next).init({
   resources: {
-    hu: { translation: hu },
-    en: { translation: en },
+    hu: { translation: huTranslation },
+    en: { translation: enTranslation },
   },
   lng: savedLang,
   fallbackLng: "en",
