eb21432000fced158cbbaebc64c0389e57563c27
Ervin, Jung <jung.ervin@gmail.com>
2026-05-25 17:09:34 +0200

    Wrap mobile app with client command center context

--- mobile/src/main.tsx
+++ mobile/src/main.tsx
@@ -4,6 +4,8 @@ import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
+import "../../client/src/i18n";
+import { CommandCenterProvider } from "../../client/src/context/CommandCenterContext";
 import App from "./App";
 import "./styles.css";
 
@@ -12,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ColorSchemeScript defaultColorScheme="dark" />
     <MantineProvider defaultColorScheme="dark">
       <Notifications position="bottom-center" autoClose={2500} />
-      <App />
+      <CommandCenterProvider>
+        <App />
+      </CommandCenterProvider>
     </MantineProvider>
   </React.StrictMode>
 );
