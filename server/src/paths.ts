311424040682f7064a37c4ca6c6571552ab717ad
Ervin, Jung <jung.ervin@gmail.com>
2026-05-25 17:02:11 +0200

    Add mobile dist path

--- server/src/paths.ts
+++ server/src/paths.ts
@@ -1,15 +1,16 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
+
 const currentDir = process.cwd();
+
 const projectRoot = currentDir.includes(`${path.sep}server`)
   ? path.resolve(currentDir, "../")
   : path.resolve(currentDir, "./");
 
 export const dataDir = path.resolve(projectRoot, "data");
-
-
 export const distDir = path.resolve(projectRoot, "./dist");
-export const clientDir = path.resolve(distDir, "./client");
+export const clientDir = path.resolve(distDir, "./client");
+export const mobileDir = path.resolve(distDir, "./mobile");
+
 export function dataFilePath(fileName: string): string {
   return path.resolve(dataDir, fileName);
 }
