8f42d18171ac61b00e3248321bc94014a7ebaae5
Ervin, Jung <jung.ervin@gmail.com>
2026-05-25 07:03:45 +0200

    Play server broadcast audio on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,9 +10,20 @@ import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import { getDefaultWsUrl } from "./services/defaultWsUrl";
 import { wsApi } from "./services/wsApi";
+import { wsClient } from "./services/wsClient";
+import { audioManager } from "./services/audioManager";
 import { LayoutContextProvider } from "./context/LayoutContextProvider";
 
 export type AppPage = "home" | "layout" | "programmer";
+
+type RuntimeWsMessage = {
+  type: string;
+  data?: unknown;
+};
+
+type PlayAudioPayload = {
+  fileName?: string;
+};
 
 export default function App() {
   const [page, setPage] = useState<AppPage>("home");
@@ -20,7 +31,23 @@ export default function App() {
   useEffect(() => {
     wsApi.connect(getDefaultWsUrl());
 
+    const unsubscribeAudio = wsClient.subscribeMessages(rawMessage => {
+      const message = rawMessage as RuntimeWsMessage;
+
+      if (message.type !== "playAudio") {
+        return;
+      }
+
+      const data = message.data as PlayAudioPayload;
+      const fileName = data.fileName?.trim();
+
+      if (fileName) {
+        audioManager.play(fileName);
+      }
+    });
+
     return () => {
+      unsubscribeAudio();
       wsApi.disconnect();
     };
   }, []);
