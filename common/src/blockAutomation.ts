7ea0b2b838105999db82839bf4fc3071ca103322
Ervin, Jung <jung.ervin@gmail.com>
2026-05-26 15:53:46 +0200

    Add block automation document types

---
+++ common/src/blockAutomation.ts
@@ -0,0 +1,42 @@
+import type {
+  BlockAction,
+  BlockActionHook,
+  BlockActionHooks,
+} from "./domainTypes.js";
+
+export type BlockAutomationBlockActions = BlockActionHooks;
+
+export type BlockAutomationDocumentDto = {
+  version: 1;
+  blocks: Record<string, BlockAutomationBlockActions>;
+};
+
+export type BlockAutomationCommandAction =
+  | "load"
+  | "save";
+
+export type BlockAutomationCommandPayload = {
+  requestId: string;
+  action: BlockAutomationCommandAction;
+  document?: BlockAutomationDocumentDto;
+};
+
+export type BlockAutomationResponsePayload = {
+  requestId: string;
+  action: BlockAutomationCommandAction;
+  ok: boolean;
+  message?: string;
+  document?: BlockAutomationDocumentDto;
+  created?: boolean;
+};
+
+export type {
+  BlockAction,
+  BlockActionHook,
+  BlockActionHooks,
+};
+
+export const createEmptyBlockAutomationDocument = (): BlockAutomationDocumentDto => ({
+  version: 1,
+  blocks: {},
+});
