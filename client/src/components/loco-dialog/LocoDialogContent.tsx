3b3d2ac3762babd8c5e512436d84e3ed734503bd
Jung Ervin <jung_ervin@princip-automatika.hu>
2026-05-26 08:25:44 +0200

    loco extended params

--- client/src/components/loco-dialog/LocoDialogContent.tsx
+++ client/src/components/loco-dialog/LocoDialogContent.tsx
@@ -88,6 +88,7 @@ export default function LocoDialogContent({
         selectedId={selectedId}
         onSelect={setSelectedId}
         onAdd={addLoco}
+        
         onDeleteSelected={deleteSelectedLoco}
         hasSelectedLoco={!!selectedLoco}
         t={t}
