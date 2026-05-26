1b2ea3bf3499c8c7f298a03bf3f6b7a6f9f5a348
Ervin, Jung <jung.ervin@gmail.com>
2026-05-26 19:24:08 +0200

    Remove temporary level crossing runtime install

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,6 @@ import { getDefaultWsUrl } from "./services/defaultWsUrl";
 import { wsApi } from "./services/wsApi";
 import { wsClient } from "./services/wsClient";
 import { playServerAudio } from "./services/serverAudioPlayback";
-import { installLevelCrossingAccessoryRuntime } from "./services/levelCrossingAccessoryRuntime";
 import { LayoutContextProvider } from "./context/LayoutContextProvider";
 
 export type AppPage = "home" | "layout" | "programmer";
@@ -32,8 +31,6 @@ export default function App() {
   useEffect(() => {
     wsApi.connect(getDefaultWsUrl());
 
-    const uninstallLevelCrossingRuntime = installLevelCrossingAccessoryRuntime();
-
     const unsubscribeAudio = wsClient.subscribeMessages(rawMessage => {
       const message = rawMessage as RuntimeWsMessage;
 
@@ -50,7 +47,6 @@ export default function App() {
     });
 
     return () => {
-      uninstallLevelCrossingRuntime();
       unsubscribeAudio();
       wsApi.disconnect();
     };
