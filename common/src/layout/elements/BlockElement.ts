e2f174922a6466e122f36c22d994654a967b34fc
Ervin, Jung <jung.ervin@gmail.com>
2026-05-26 15:55:00 +0200

    Remove block actions from common block element

--- common/src/layout/elements/BlockElement.ts
+++ common/src/layout/elements/BlockElement.ts
@@ -1,6 +1,3 @@
-import type {
-  BlockActionHooks,
-} from "../../domainTypes.js";
 import {
   BLOCK_TYPES,
   type BlockType,
@@ -26,7 +23,6 @@ export class BlockElement extends TrackElement {
   length: number = 1;
   sensorAddress: number = 0;
   blockType: BlockType = BLOCK_TYPES.NORMAL;
-  actions: BlockActionHooks = {};
 
   constructor(x: number, y: number) {
     super(x, y);
@@ -87,7 +83,6 @@ export class BlockElement extends TrackElement {
     element.sensorAddress = data.sensorAddress ?? 0;
     element.locoAddress = data.locoAddress ?? 0;
     element.blockType = data.blockType ?? BLOCK_TYPES.NORMAL;
-    element.actions = data.actions ?? {};
     return element;
   }
 
@@ -99,7 +94,6 @@ export class BlockElement extends TrackElement {
       length: this.length,
       sensorAddress: this.sensorAddress,
       blockType: this.blockType,
-      actions: this.actions,
     };
   }
 }
