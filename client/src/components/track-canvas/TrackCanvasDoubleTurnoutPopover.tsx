8cd784f50386122ff512150b8d77c7d0d107033c
Ervin, Jung <jung.ervin@gmail.com>
2026-05-27 12:02:02 +0200

    Align double turnout popover with signal popover layout

--- client/src/components/track-canvas/TrackCanvasDoubleTurnoutPopover.tsx
+++ client/src/components/track-canvas/TrackCanvasDoubleTurnoutPopover.tsx
@@ -184,6 +184,7 @@ export function TrackCanvasDoubleTurnoutPopover({
                     label={position.label}
                     width={40}
                     height={40}
+                    translateX={-10}
                   />
                 </Box>
               );
