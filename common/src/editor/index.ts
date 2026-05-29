1ae7fedd022fe151b4a8874e8ac2ec0a36512005
Ervin, Jung <jung.ervin@gmail.com>
2026-05-29 17:22:02 +0200

    Add NodeNext extensions to editor barrel exports

--- common/src/editor/index.ts
+++ common/src/editor/index.ts
@@ -1,9 +1,9 @@
-export * from "./core/types";
-export * from "./core/BaseElement";
-export * from "./core/Layer";
-export * from "./core/Layout";
-export * from "./core/ElementFactory";
+export * from "./core/types.js";
+export * from "./core/BaseElement.js";
+export * from "./core/Layer.js";
+export * from "./core/Layout.js";
+export * from "./core/ElementFactory.js";
 
-export * from "./elements/UnknownElement";
-export * from "./elements/TrackElement";
-export * from "./elements/TrackStraightElement";
+export * from "./elements/UnknownElement.js";
+export * from "./elements/TrackElement.js";
+export * from "./elements/TrackStraightElement.js";
