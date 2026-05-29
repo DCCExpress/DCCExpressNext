094f87544c0f70663af61c8dba809cf123ed042c
Ervin, Jung <jung.ervin@gmail.com>
2026-05-29 17:21:02 +0200

    Fix TrackElement imports for NodeNext

--- common/src/editor/elements/TrackElement.ts
+++ common/src/editor/elements/TrackElement.ts
@@ -1,5 +1,5 @@
-import { BaseElement } from "../core/BaseElement";
-import type { ElementJSON } from "../core/types";
+import { BaseElement } from "../core/BaseElement.js";
+import type { ElementJSON } from "../core/types.js";
 
 export type TrackElementJSON = ElementJSON & {
   layerId: "track";
@@ -29,8 +29,9 @@ export abstract class TrackElement<TJSON extends TrackElementJSON = TrackElement
   }
 
   protected trackToJSON(): TrackElementJSON {
+    const json = this.baseToJSON();
     return {
-      ...this.baseToJSON(),
+      ...json,
       layerId: "track",
       sectionId: this.sectionId,
     };
