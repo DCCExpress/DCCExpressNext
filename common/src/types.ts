5db0ec2ab3d95e150f85addcc7ee2863dbb6a6b4
Ervin, Jung <jung.ervin@gmail.com>
2026-07-08 08:50:58 +0200

    Export automation flow shared types

--- common/src/types.ts
+++ common/src/types.ts
@@ -23,3 +23,4 @@ export * from "./appSettings.js";
 export * from "./serverRuntimeStats.js";
 export * from "./runtimeVariables.js";
 export * from "./blockAutomation.js";
+export * from "./automationFlow.js";
