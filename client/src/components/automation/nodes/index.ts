f258f441c641ab990b16e5981a28ea468cab0dc9
Ervin, Jung <jung.ervin@gmail.com>
2026-07-08 09:33:32 +0200

    Export automation node components

---
+++ client/src/components/automation/nodes/index.ts
@@ -0,0 +1,12 @@
+export {
+  NODE_DEFINITIONS,
+  NODE_GROUPS,
+  hasSourceHandle,
+  hasTargetHandle,
+  isInputNode,
+} from "./automationNodeDefinitions";
+
+export {
+  AutomationGraphNode,
+  automationNodeTypes,
+} from "./AutomationGraphNode";
